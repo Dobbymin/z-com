48fb309ef50a390a9421c845e7626a926e0363c7
Dobbymin <gmin990624@gmail.com>
2025-05-23 19:03:28 +0900

    init: 파일 구조 생성

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,38 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import unusedImports from 'eslint-plugin-unused-imports';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends(
+		'next/core-web-vitals',
+		'next/typescript',
+		'plugin:react/jsx-runtime',
+		'prettier'
+	),
+	{
+		plugins: {
+			'unused-imports': unusedImports,
+		},
+		rules: {
+			'react/jsx-sort-props': [
+				'warn',
+				{
+					callbacksLast: true,
+					shorthandFirst: true,
+					noSortAlphabetically: false,
+					reservedFirst: true,
+				},
+			],
+		},
+	},
 ];
 
 export default eslintConfig;
