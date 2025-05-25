89cccc07bcf252258c60be74410cedba8ad4c123
Dobbymin <gmin990624@gmail.com>
2025-05-25 19:47:13 +0900

    refactor: scss 파일 적용 및 코드 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-import './globals.css';
-import styles from './page.module.css';
+import './globals.scss';
+import styles from './page.module.scss';
 
 const inter = Inter({ subsets: ['latin'] });
 
